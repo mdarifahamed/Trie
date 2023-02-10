e2f539a7ac87223db0791168152ab6a3a881c857
Arif <arifonly4@gmail.com>
2023-02-10 19:13:49 +0530

    Trie

---
+++ PrefixP.java
@@ -0,0 +1,48 @@
+public class PrefixP {
+    static class Node {
+        Node[] children = new Node[26];
+        boolean eow = false;
+        int freq;
+
+        public Node() {
+            for(int i=0; i<children.length; i++) {
+                children[i] = null;
+            }
+            freq = 1;
+        }
+    }
+
+    public static Node root = new Node();
+
+    // insert
+    public static void insert(String word) {
+        Node curr = root;
+        for(int i=0; i<word.length(); i++) {
+            int idx = word.charAt(i)-'a';
+            if(curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            } else {
+                curr.children[idx].freq++;
+            }
+
+            curr = curr.children[idx];
+        }
+
+
+        curr.eow = true;
+    }
+
+    // findPrefix
+    public static void findPrefix(String word, String ans, int idx) {
+        
+    }
+
+    public static void main(String[] args) {
+        String arr[] = {"zebra", "dog", "duck", "dove"};
+        for(int i=0; i<arr.length; i++) {
+            insert(arr[i]);
+        }
+
+        root.freq = -1;
+    }
+}
